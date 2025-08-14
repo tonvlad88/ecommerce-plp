095149def19ebeefd63aa05effdb878b98d765bb
Tony Saromines <tonysaromines@gmail.com>
2025-08-14 22:11:34 +0800

    Refactor product card

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Loading from "@/components/loading";
+import ProductCard from "@/components/productCard";
 import { Product } from "@/types/products";
 import { useEffect, useState } from "react";
 
@@ -26,33 +27,9 @@ export default function ProductsPage() {
   return (
     <div className="p-6 max-w-5xl mx-auto">
       <h1 className="text-3xl font-bold mb-6">🛍️ Product Listing</h1>
-      {/* Product Grid */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
         {products.map((product: Product) => (
-          <div
-            key={product.id}
-            className="relative h-64 rounded-lg overflow-hidden shadow-lg group"
-          >
-            {/* Background Image with Zoom on Hover */}
-            <div
-              className="absolute inset-0 bg-cover bg-center transition-transform duration-500 group-hover:scale-105"
-              style={{ backgroundImage: `url(${product.thumbnail})` }}
-            />
-
-            {/* Gradient Overlay */}
-            <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent opacity-60 group-hover:opacity-80 transition duration-300" />
-
-            {/* Category Badge */}
-            <div className="absolute top-3 left-3 bg-white text-indigo-600 text-xs font-semibold px-3 py-1 rounded-full shadow-md z-10">
-              {product.category}
-            </div>
-
-            {/* Text Content */}
-            <div className="absolute bottom-0 p-4 text-white z-10">
-              <h2 className="text-lg font-semibold">{product.title}</h2>
-              <p className="text-blue-200 font-bold mt-1">${product.price}</p>
-            </div>
-          </div>
+          <ProductCard product={product} key={product.id} />
         ))}
       </div>
     </div>
---
+++ components/productCard.tsx
@@ -0,0 +1,30 @@
+import { Product } from "@/types/products";
+
+export default function ProductCard({ product }: { product: Product }) {
+  return (
+    <div
+      key={product.id}
+      className="relative h-64 rounded-lg overflow-hidden shadow-lg group"
+    >
+      {/* Background Image with Zoom on Hover */}
+      <div
+        className="absolute inset-0 bg-cover bg-center transition-transform duration-500 group-hover:scale-105"
+        style={{ backgroundImage: `url(${product.thumbnail})` }}
+      />
+
+      {/* Gradient Overlay */}
+      <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent opacity-60 group-hover:opacity-80 transition duration-300" />
+
+      {/* Category Badge */}
+      <div className="absolute top-3 left-3 bg-white text-indigo-600 text-xs font-semibold px-3 py-1 rounded-full shadow-md z-10">
+        {product.category}
+      </div>
+
+      {/* Text Content */}
+      <div className="absolute bottom-0 p-4 text-white z-10">
+        <h2 className="text-lg font-semibold">{product.title}</h2>
+        <p className="text-blue-200 font-bold mt-1">${product.price}</p>
+      </div>
+    </div>
+  );
+}
