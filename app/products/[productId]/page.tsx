895c1e424436aee6cfd5ac91a48aa7061f811fd6
Tony Saromines <tonysaromines@gmail.com>
2025-12-12 23:40:58 +0800

    Add horizontal and responsive iframes to product page
    Introduced two new sections to the product details page: one displaying a horizontal iframe and another displaying a responsive iframe, both embedding external widgets with specific category and status filters.

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -61,6 +61,32 @@ export default async function ProductDetails({
           <BuyButton priceId="price_1S4PHv2dtCcwsNdzj0WYvygs" quantity={1} />
         </div>
       </div>
+
+      <div className="mt-4">
+        <h2 className="text-2xl font-bold mb-4 text-center">
+          HORIZONTAL IFRAME
+        </h2>
+        <iframe
+          src="http://live.spago.com/shows?primaryCategory=%255B%2522shoes%2522%252C%2522wallets%2522%252C%2522skin_care%2522%252C%2522other%2522%255D&subCategory=%255B%2522heels%2522%252C%2522ballet_flats%2522%252C%2522sneakers%2522%252C%2522espadrilles%2522%252C%2522boots%2522%252C%2522loafers%2522%252C%2522slippers%2522%252C%2522slingback%2522%252C%2522mary_janes%2522%252C%2522large_wallets%2522%252C%2522wristlets%2522%252C%2522cardholders%2522%252C%2522self_tanning%2522%252C%2522cleaners_and_exfoliators%2522%252C%2522hair_removal%2522%252C%2522bath_and_shower%2522%252C%2522lip_care%2522%252C%2522skin_care_tools%2522%255D&status=%255B%2522scheduled%2522%252C%2522live%2522%252C%2522ended%2522%252C%2522cancelled%2522%255D"
+          width="100%"
+          height="530"
+          title="Widget Preview"
+          allowFullScreen
+        ></iframe>
+      </div>
+
+      <div className="mt-10">
+        <h2 className="text-2xl font-bold mb-4 text-center">
+          RESPONSIVE IFRAME
+        </h2>
+        <iframe
+          src="http://live.spago.com/embed/widget?widgetId=c85e5236-e0ea-4714-835b-bf8199b7bf92&primaryCategory=%255B%2522shoes%2522%252C%2522wallets%2522%252C%2522skin_care%2522%252C%2522other%2522%255D&subCategory=%255B%2522heels%2522%252C%2522ballet_flats%2522%252C%2522sneakers%2522%252C%2522espadrilles%2522%252C%2522boots%2522%252C%2522loafers%2522%252C%2522slippers%2522%252C%2522slingback%2522%252C%2522mary_janes%2522%252C%2522large_wallets%2522%252C%2522wristlets%2522%252C%2522cardholders%2522%252C%2522self_tanning%2522%252C%2522cleaners_and_exfoliators%2522%252C%2522hair_removal%2522%252C%2522bath_and_shower%2522%252C%2522lip_care%2522%252C%2522skin_care_tools%2522%255D&status=%255B%2522scheduled%2522%252C%2522live%2522%252C%2522ended%2522%252C%2522cancelled%2522%255D"
+          width="100%"
+          height="530"
+          title="Widget Preview"
+          allowFullScreen
+        ></iframe>
+      </div>
     </div>
   );
 }
